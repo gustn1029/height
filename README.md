3dea6d4cc8edde9f27ac87a11e2a8109e4909c58
hyunsoo <gustn1029@gmail.com>
2021-04-05 17:14:01 +0900

    'height-1day02'

---
+++ README.md
@@ -0,0 +1,1 @@
+Vanilla JavaScript - 특정 요소 위치로 스크롤 이동  
--- main.js
+++ main.js
@@ -7,11 +7,9 @@ const container = document.querySelector('.container');
 console.log(container.scrollHeight);
 
 for(let i = 0; i < heightLen; i++) {
-    const docHeight = height[i].clientHeight;
-    console.log(container.scrollHeight - height[i].scrollHeight);
     clickBtn[i].addEventListener('click', function(e){
         e.preventDefault();
         console.log(height[i]);
-        height[i].scrollTo(0,0);
+        height[i].scrollIntoView({behavior: 'smooth'});
     });
 }
